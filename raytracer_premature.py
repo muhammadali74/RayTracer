c0ce2b323d1877a313740eb2932651e16d660776
Syed Muhammad Ali Naqvi <sn07590@st.habib.edu.pk>
2023-04-04 17:33:05 +0500

    Update raytracer_premature.py

--- raytracer_premature.py
+++ raytracer_premature.py
@@ -3,7 +3,24 @@ import matplotlib.pyplot as plt
 
 
 class Sphere(object):
-    def
+    def __init__(self, center, radius, ambient, diffuse, specular, shininess, reflection):
+        self.center = center
+        self.radius = radius
+        self.ambient = ambient
+        self.diffuse = diffuse
+        self.specular = specular
+        self.shininess = shininess
+        self.reflection = reflection
+        self.bounds = np.array([center - radius, center + radius])
+
+
+class Light(object):
+    def __init__(self, position, ambient, diffuse, specular):
+        self.position = position
+        self.ambient = ambient
+        self.diffuse = diffuse
+        self.specular = specular
+
 
 def normalize(vector):
     return vector / np.linalg.norm(vector)
@@ -27,7 +44,7 @@ def sphere_intersect(center, radius, ray_origin, ray_direction):
 
 def nearest_intersected_object(objects, ray_origin, ray_direction):
     distances = [sphere_intersect(
-        obj['center'], obj['radius'], ray_origin, ray_direction) for obj in objects]
+        obj.center, obj.radius, ray_origin, ray_direction) for obj in objects]
     # print(distances)
     nearest_object = None
     min_distance = np.inf
@@ -47,33 +64,24 @@ camera = np.array([0, 0, 1])
 ratio = float(width) / height
 screen = (-1, 1 / ratio, 1, -1 / ratio)  # left, top, right, bottom
 
-light = {'position': np.array([5, 5, 5]), 'ambient': np.array(
-    [1, 1, 1]), 'diffuse': np.array([1, 1, 1]), 'specular': np.array([1, 1, 1])}
 
-# objects = [
-#     {'center': np.array([-0.2, 0, -1]), 'radius': 0.7,
-#      'color': np.array([1, 0, 0])},
-#     {'center': np.array([0.1, -0.3, 0]), 'radius': 0.1,
-#      'color':np.array([0, 1, 0])},
-#     {'center': np.array([-0.3, 0, 0]), 'radius': 0.15,
-#      'color': np.array([0, 0, 1])},
-#     {'center': np.array([0, -9000, 0]), 'radius': 9000 - 0.7, 'ambient': np.array([0.1, 0.1, 0.1]),
-#      'diffuse': np.array([0.6, 0.6, 0.6]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5, 'color': np.array([1, 1, 1])}
-# ]
+light = Light(np.array([5, 5, 5]), np.array([1, 1, 1]),
+              np.array([1, 1, 1]), np.array([1, 1, 1]))
 
 objects = [
-    {'center': np.array([-0.2, 0, -1]), 'radius': 0.7, 'ambient': np.array([0.1, 0, 0]), 'diffuse': np.array(
-        [0.7, 0, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5},
-    {'center': np.array([0.1, -0.3, 0]), 'radius': 0.1, 'ambient': np.array([0.1, 0, 0.1]), 'diffuse': np.array(
-        [0.7, 0, 0.7]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5},
-    {'center': np.array([-0.3, 0, 0]), 'radius': 0.15, 'ambient': np.array([0, 0.1, 0]), 'diffuse': np.array(
-        [0, 0.6, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5},
-    {'center': np.array([-0.5, 0.5, -0.5]), 'radius': 0.15, 'ambient': np.array([0, 0.1, 0.6]), 'diffuse': np.array(
-        [0, 0.6, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5},
-    {'center': np.array([-0.3, 0.4, -2]), 'radius': 0.9, 'ambient': np.array([0.2, 0.1, 0]), 'diffuse': np.array(
-        [0, 0.6, 0]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5},
-    {'center': np.array([0, -9000, 0]), 'radius': 9000 - 0.7, 'ambient': np.array([0.1, 0.1, 0.1]),
-     'diffuse': np.array([0.6, 0.6, 0.6]), 'specular': np.array([1, 1, 1]), 'shininess': 100, 'reflection': 0.5}
+    Sphere(np.array([-0.2, 0, -1]), 0.7, np.array([0.1, 0, 0]),
+           np.array([0.7, 0, 0]), np.array([1, 1, 1]), 100, 0.5),
+    Sphere(np.array([0.1, -0.3, 0]),  0.1, np.array([0.1, 0, 0.1]),
+           np.array([0.7, 0, 0.7]),  np.array([1, 1, 1]),  100, 0.5),
+    Sphere(np.array([-0.3, 0, 0]), 0.15, np.array([0, 0.1, 0]),
+           np.array([0, 0.6, 0]), np.array([1, 1, 1]), 100, 0.5),
+    Sphere(np.array([-0.5, 0.5, -0.5]), 0.15, np.array([0, 0.1, 0.6]),
+           np.array([0, 0.6, 0]), np.array([1, 1, 1]), 100, 0.5),
+    Sphere(np.array([-0.3, 0.4, -2]), 0.9, np.array([0.2, 0.1, 0]),
+           np.array([0, 0.6, 0]), np.array([1, 1, 1]), 100, 0.5),
+    Sphere(np.array([0, -9000, 0]), 9000 - 0.7, np.array([0.1, 0.1, 0.1]),
+           np.array([0.6, 0.6, 0.6]), np.array([1, 1, 1]), 100, 0.5),
+
 ]
 
 # root = build_bvh(objects)
@@ -100,10 +108,10 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
 
             intersection = origin + min_distance * direction
             normal_to_surface = normalize(
-                intersection - nearest_object['center'])
+                intersection - nearest_object.center)
             shifted_point = intersection + 1e-5 * normal_to_surface
             intersection_to_light = normalize(
-                light['position'] - shifted_point)
+                light.position - shifted_point)
 
             # _, min_distance = nearest_intersected_object(
             #     objects, shifted_point, intersection_to_light)
@@ -117,17 +125,17 @@ for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
             illumination = np.zeros((3))
 
             # ambiant
-            illumination += nearest_object['ambient'] * light['ambient']
+            illumination += nearest_object.ambient * light.ambient
 
             # diffuse
-            illumination += nearest_object['diffuse'] * light['diffuse'] * \
+            illumination += nearest_object.diffuse * light.diffuse * \
                 np.dot(intersection_to_light, normal_to_surface)
 
             # specular
             intersection_to_camera = normalize(camera - intersection)
             H = normalize(intersection_to_light + intersection_to_camera)
-            illumination += nearest_object['specular'] * light['specular'] * np.dot(
-                normal_to_surface, H) ** (nearest_object['shininess'] / 4)
+            illumination += nearest_object.specular * light.specular * np.dot(
+                normal_to_surface, H) ** (nearest_object.shininess / 4)
 
             # reflection
             color += illumination
