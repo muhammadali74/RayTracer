7bd0ed82bf00229c5f7f348b4b3e7de65276e722
Syed Muhammad Ali Naqvi <sn07590@st.habib.edu.pk>
2023-04-08 17:13:42 +0500

    some serious shit

--- raytracer_thoramature.py
+++ raytracer_thoramature.py
@@ -71,8 +71,8 @@ def nearest_intersected_object(objects, ray_origin, ray_direction):
     return nearest_object, min_distance
 
 
-width = 300
-height = 200
+width = 1920
+height = 1080
 
 max_depth = 3
 
@@ -181,78 +181,78 @@ end = time.time()
 print(end - start)
 
 
-image = np.zeros((height, width, 3))
-# plt.show()
-start = time.time()
+# image = np.zeros((height, width, 3))
+# # plt.show()
+# start = time.time()
 
-for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
-    for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
-        # screen is on origin
-        pixel = np.array([x, y, 0])
-        origin = camera
-        direction = normalize(pixel - origin)
+# for i, y in enumerate(np.linspace(screen[1], screen[3], height)):
+#     for j, x in enumerate(np.linspace(screen[0], screen[2], width)):
+#         # screen is on origin
+#         pixel = np.array([x, y, 0])
+#         origin = camera
+#         direction = normalize(pixel - origin)
 
-        color = np.zeros((3))
-        reflection = 1
+#         color = np.zeros((3))
+#         reflection = 1
 
-        for k in range(max_depth):
-            # check for intersections
-            nearest_object, min_distance = tree.traverse(origin, direction)
-            # nearest_object, min_distance = nearest_intersected_object(
-            #     objects, origin, direction)
+#         for k in range(max_depth):
+#             # check for intersections
+#             nearest_object, min_distance = tree.traverse(origin, direction)
+#             # nearest_object, min_distance = nearest_intersected_object(
+#             #     objects, origin, direction)
 
-            if nearest_object is None:
-                break
+#             if nearest_object is None:
+#                 break
 
-            intersection = origin + min_distance * direction
-            normal_to_surface = normalize(
-                intersection - nearest_object.center)
-            shifted_point = intersection + 1e-5 * normal_to_surface
-            intersection_to_light = normalize(
-                light.position - shifted_point)
+#             intersection = origin + min_distance * direction
+#             normal_to_surface = normalize(
+#                 intersection - nearest_object.center)
+#             shifted_point = intersection + 1e-5 * normal_to_surface
+#             intersection_to_light = normalize(
+#                 light.position - shifted_point)
 
-            # _, min_distance = nearest_intersected_object(
-            #     objects, shifted_point, intersection_to_light)
-            _, min_distance = tree.traverse(
-                shifted_point, intersection_to_light)
-            intersection_to_light_distance = np.linalg.norm(
-                light.position - intersection)
-            is_shadowed = min_distance < intersection_to_light_distance
+#             # _, min_distance = nearest_intersected_object(
+#             #     objects, shifted_point, intersection_to_light)
+#             _, min_distance = tree.traverse(
+#                 shifted_point, intersection_to_light)
+#             intersection_to_light_distance = np.linalg.norm(
+#                 light.position - intersection)
+#             is_shadowed = min_distance < intersection_to_light_distance
 
-            if is_shadowed:
-                break
+#             if is_shadowed:
+#                 break
 
-            illumination = np.zeros((3))
+#             illumination = np.zeros((3))
 
-            # ambiant
-            illumination += nearest_object.ambient * light.ambient
+#             # ambiant
+#             illumination += nearest_object.ambient * light.ambient
 
-            # diffuse
-            illumination += nearest_object.diffuse * light.diffuse * \
-                np.dot(intersection_to_light, normal_to_surface)
+#             # diffuse
+#             illumination += nearest_object.diffuse * light.diffuse * \
+#                 np.dot(intersection_to_light, normal_to_surface)
 
-            # specular
-            intersection_to_camera = normalize(camera - intersection)
-            H = normalize(intersection_to_light + intersection_to_camera)
-            illumination += nearest_object.specular * light.specular * np.dot(
-                normal_to_surface, H) ** (nearest_object.shininess / 4)
+#             # specular
+#             intersection_to_camera = normalize(camera - intersection)
+#             H = normalize(intersection_to_light + intersection_to_camera)
+#             illumination += nearest_object.specular * light.specular * np.dot(
+#                 normal_to_surface, H) ** (nearest_object.shininess / 4)
 
-            # reflection
-            color += reflection * illumination
-            reflection *= nearest_object.reflection
+#             # reflection
+#             color += reflection * illumination
+#             reflection *= nearest_object.reflection
 
-            origin = shifted_point
-            direction = reflected(direction, normal_to_surface)
+#             origin = shifted_point
+#             direction = reflected(direction, normal_to_surface)
 
-        image[i, j] = np.clip(color, 0, 1)
+#         image[i, j] = np.clip(color, 0, 1)
 
-    print("%d/%d" % (i + 1, height))
-    # myobj.set_data(image)
-    # plt.show()
+#     print("%d/%d" % (i + 1, height))
+#     # myobj.set_data(image)
+#     # plt.show()
 
 
-end = time.time()
-print(end - start)
+# end = time.time()
+# print(end - start)
 
 
 plt.imsave('imagenew.png', image)
